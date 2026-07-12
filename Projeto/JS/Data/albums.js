3024c0599bcf83575e2680cfcba11ebdaa8e24e6
Gabriel Pereira <gabs.pereira0@icloud.com>
2026-07-12 17:12:28 -0300

    adicionado novos artistas e albums

---
+++ Projeto/JS/Data/albums.js
@@ -0,0 +1,72 @@
+export const albumsData = [
+  {
+    name: 'Beerbongs & Bentleys',
+    artist: 'Post Malone',
+    Image: './IMG/album-beerbongs-bentleys.jpg',
+  },
+  {
+    name: 'Dias Antes do Caos',
+    artist: 'Alee',
+    Image: './IMG/album-dias-antes-do-caos.jpg',
+  },
+  {
+    name: 'Eu Odeio o Dia dos Namorados',
+    artist: 'Orochi',
+    Image: './IMG/album-eu-odeio-o-dia-dos-namorados.jpg',
+  },
+  {
+    name: 'Fkay',
+    artist: 'Kayblack, Fulgo FK',
+    Image: './IMG/album-fkay.jpg',
+  },
+  {
+    name: 'GNX',
+    artist: 'Kendrick Lamar',
+    Image: './IMG/album-gnx.jpg',
+  },
+  {
+    name: 'Heroes Villains',
+    artist: 'Metro Boomin',
+    Image: './IMG/album-heroes-villains.jpg',
+  },
+  {
+    name: 'Icarus',
+    artist: 'BK',
+    Image: './IMG/album-icarus.jpg',
+  },
+  {
+    name: 'Iceman',
+    artist: 'Drake',
+    Image: './IMG/album-iceman.jpg',
+  },
+  {
+    name: 'Kodak the Blessing',
+    artist: 'Kodak Black',
+    Image: './IMG/album-kodak-the-blessing.jpg',
+  },
+  {
+    name: 'Maquina do Tempo',
+    artist: 'Matue',
+    Image: './IMG/album-maquina-do-tempo.jpg',
+  },
+  {
+    name: 'Gostosas Ouvem Tz',
+    artist: 'Tz da Coronel',
+    Image: './IMG/album-mixtape-de-ver-o-gostosas-ouvem-tz.jpg',
+  },
+  {
+    name: 'Nume',
+    artist: 'Filipe Ret',
+    Image: './IMG/album-nume-ep-logo.jpg',
+  },
+  {
+    name: 'Octane',
+    artist: 'Don Toliver',
+    Image: './IMG/album-octane.jpg',
+  },
+  {
+    name: 'Utopia',
+    artist: 'Travis Scott',
+    Image: './IMG/album-utopia.jpg',
+  },
+];
---
+++ Projeto/JS/Data/paradas.js
@@ -0,0 +1,77 @@
+export const paradasData = [
+  {
+    name: 'Funk Viral',
+    description: 'ginga y balança',
+    Image: './IMG/album-funk-viral.jpg',
+  },
+  {
+    name: 'Paredão Explode',
+    description:
+      'Os hits do forró, arrocha, pagodão e muito mais! Foto: Wesley Safadão',
+    Image: './IMG/album-paredao-explode.jpg',
+  },
+  {
+    name: 'Gigantes do Rap Nacional',
+    description: 'Ouça grandes MCs do hip hop nacional. Foto: Da Guedes',
+    Image: './IMG/album-gigantes-do-rap-nacional.jpg',
+  },
+  {
+    name: 'Aulas',
+    description: 'estatística não esconde lírica comum // Rincon Sapiência',
+    Image: './IMG/album-aulas.jpg',
+  },
+  {
+    name: 'Top 50 - Brasil',
+    description:
+      'Brasil: seu relatório diário das faixas mais tocadas no momento.',
+    Image: './IMG/album-top-50-brazil.jpg',
+  },
+  {
+    name: 'Top 50 - Global',
+    description: 'Faixas mais tocadas no momento.',
+    Image: './IMG/album-top-50-global.jpg',
+  },
+  {
+    name: 'Top Brasil',
+    description: 'Os maiores hits do país. O que tá bombando tá aqui.',
+    Image: './IMG/album-top-brasil.jpg',
+  },
+  {
+    name: 'Top Músicas do Mundo',
+    description:
+      'Mundo: seu relatório semanal das faixas mais tocadas no momento.',
+    Image: './IMG/album-top-songs-global.jpg',
+  },
+  {
+    name: 'Top Músicas Brasil',
+    description:
+      'Brasil: seu relatório semanal das faixas mais tocadas no momento.',
+    Image: './IMG/album-top-songs-brasil.jpg',
+  },
+  {
+    name: 'Projeto Verão',
+    description:
+      'A melhor trilha sonora pra se preparar pros dias mais quentes do ano!',
+    Image: './IMG/album-projeto-verao.jpg',
+  },
+  {
+    name: 'Rap Caviar',
+    description: 'New music by Future and J. Cole.',
+    Image: './IMG/album-rapcaviar.jpg',
+  },
+  {
+    name: "I Love My'90s Hip Hop",
+    description: 'Real rap music from the golden era.',
+    Image: './IMG/album-i-love-my-90s-hip-hop.jpg',
+  },
+  {
+    name: 'O Trap Mais Caro',
+    description: 'e aí tue, como foi o show?',
+    Image: './IMG/album-o-trap-mais-caro.jpg',
+  },
+  {
+    name: 'Trap Classe',
+    description: 'O trap nacional pra qualquer hora, daquele jeitão',
+    Image: './IMG/album-trap-classe.jpg',
+  },
+];
